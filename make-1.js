f5bcb3c531c2a402e19bf4107496d825e1b982a3
Jesse Rice <hobstweedl@gmail.com>
2019-11-19 23:34:03 -0500

    fix a typo

--- make-1.js
+++ make-1.js
@@ -3,7 +3,7 @@ const make = (r, c) => {
   for (let y = 0; y < c; y++) {
     matrix[y] = [];
     for (let x = 0; x < r; x++) {
-      board[y][x] = 0;
+      matrix[y][x] = 0;
     }
   }
   return matrix;
